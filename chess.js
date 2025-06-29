e104d6f1cc44fd372f04dddf57e428f1cb5c9178
Chukwuii <109396213+Chukwuii@users.noreply.github.com>
2025-06-29 09:48:16 +0100

    Create chess.js

---
+++ chess.js
@@ -0,0 +1,19 @@
+const board = [
+    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+];
+// Note: uppercase for white, lowercase for black.
+
+// Or we can use objects for each square: { type: 'pawn', color: 'white' }
+// But for simplicity, we'll use the above and map to unicode.
+
+const pieceToUnicode = {
+    'r': '♜', 'n': '♞', 'b': '♝', 'q': '♛', 'k': '♚', 'p': '♟',
+    'R': '♖', 'N': '♘', 'B': '♗', 'Q': '♕', 'K': '♔', 'P': '♙'
+};
